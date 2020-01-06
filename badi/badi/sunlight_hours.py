fcaddcf77ac58f5c9cfe3fd47f98d5664827cdc4
Carmelo <carmelo.acosta@sage.com>
2020-01-06 11:30:55 +0100

    Fixing get_neighbourhood_sunlight_hours

--- badi/badi/sunlight_hours.py
+++ badi/badi/sunlight_hours.py
@@ -4,6 +4,7 @@
     Module that gathers tools to compute sunlight hours.
 """
 
+from math import atan, atan2, degrees
 
 from .constants import FLOOR_HEIGHT
 
@@ -28,13 +29,13 @@ def get_apartment_dawn(angle, city_seconds_per_grade, city_dawn):
 
                 Examples: '08:14', '17:25'
 
-    :return: (str) The local time when starts the sunlight in the apartment. Follows the same format than city_dawn.
+    :return: (str) The local time when starts the sunlight in the apartment. Follows the same format as city_dawn.
     """
     elapsed_seconds_from_dawn = int(angle * city_seconds_per_grade)
     elapsed_hours_from_dawn = 0
     elapsed_minutes_from_dawn = int(elapsed_seconds_from_dawn/60)
 
-    if elapsed_minutes_from_dawn > 59:
+    while elapsed_minutes_from_dawn > 59:
         elapsed_hours_from_dawn = int(elapsed_minutes_from_dawn/60)
         elapsed_minutes_from_dawn -= elapsed_hours_from_dawn * 60
 
@@ -44,11 +45,11 @@ def get_apartment_dawn(angle, city_seconds_per_grade, city_dawn):
     apartment_dawn_hour_int = city_dawn_hour_int + elapsed_hours_from_dawn
     apartment_dawn_minute_int = city_dawn_minute_int + elapsed_minutes_from_dawn
 
-    if apartment_dawn_minute_int > 59:
-        apartment_dawn_minute_int = 60 - apartment_dawn_minute_int
+    while apartment_dawn_minute_int > 59:
+        apartment_dawn_minute_int = apartment_dawn_minute_int - 60
         apartment_dawn_hour_int += 1
 
-    result = "{}:{}".format(str(apartment_dawn_hour_int).rjust(2, '0'), apartment_dawn_minute_int)
+    result = "{}:{}".format(str(apartment_dawn_hour_int).rjust(2, '0'), str(apartment_dawn_minute_int).rjust(2, '0'))
 
     return result
 
@@ -64,7 +65,7 @@ def get_apartment_sunset(angle, city_seconds_per_grade, city_sunset):
         RECALL that, following the Code Challenge definition, the total number of seconds elapsed from the city dawn
         to the city sunset are equally distributed between the 180 grades that cover the full dawn-sunset range.
 
-    :param city_sunset: (str) The local time when starts the sunlight in the apartment. Follows the format:
+    :param city_sunset: (str) The local time when ends the sunlight in the apartment. Follows the format:
 
                                             HH:MM
                 with
@@ -73,32 +74,132 @@ def get_apartment_sunset(angle, city_seconds_per_grade, city_sunset):
 
                 Examples: '08:14', '17:25'
 
-    :return: (str) The local time when starts the sunlight in the apartment. Follows the same format than city_sunset.
+    :return: (str) The local time when starts the sunlight in the apartment. Follows the same format as city_sunset.
     """
-    elapsed_seconds_from_dawn = int(angle * city_seconds_per_grade)
-    elapsed_hours_from_dawn = 0
-    elapsed_minutes_from_dawn = int(elapsed_seconds_from_dawn/60)
+    remaining_seconds_until_sunset = int(angle * city_seconds_per_grade)
+    remaining_hours_until_sunset = 0
+    remaining_minutes_until_sunset = int(remaining_seconds_until_sunset/60)
 
-    if elapsed_minutes_from_dawn > 59:
-        elapsed_hours_from_dawn = int(elapsed_minutes_from_dawn/60)
-        elapsed_minutes_from_dawn -= elapsed_hours_from_dawn * 60
+    while remaining_minutes_until_sunset > 59:
+        remaining_hours_until_sunset = int(remaining_minutes_until_sunset/60)
+        remaining_minutes_until_sunset -= remaining_hours_until_sunset * 60
 
-    city_dawn_hour_int = int(city_sunset.split(':')[0])
-    city_dawn_minute_int = int(city_sunset.split(':')[1])
+    city_sunset_hour_int = int(city_sunset.split(':')[0])
+    city_sunset_minute_int = int(city_sunset.split(':')[1])
 
-    apartment_dawn_hour_int = city_dawn_hour_int - elapsed_hours_from_dawn
-    apartment_dawn_minute_int = city_dawn_minute_int - elapsed_minutes_from_dawn
+    apartment_sunset_hour_int = city_sunset_hour_int - remaining_hours_until_sunset
+    apartment_sunset_minute_int = city_sunset_minute_int - remaining_minutes_until_sunset
 
-    if apartment_dawn_minute_int < 0:
-        apartment_dawn_minute_int = 60 + apartment_dawn_minute_int
-        apartment_dawn_hour_int -= 1
+    while apartment_sunset_minute_int < 0:
+        apartment_sunset_minute_int = apartment_sunset_minute_int + 60
+        apartment_sunset_hour_int -= 1
 
-    result = "{}:{}".format(str(apartment_dawn_hour_int).rjust(2, '0'), apartment_dawn_minute_int)
+    result = "{}:{}".format(str(apartment_sunset_hour_int).rjust(2, '0'),
+                            str(apartment_sunset_minute_int).rjust(2, '0'))
 
     return result
 
 
-def get_neighbourhood_sunlight_hours(building_list):
+def elapsed_time(start_time, end_time):
+    """
+        Returns the time elapsed in the interval comprised between start_time and end_time.
+
+    :param start_time: (str) The start time of the interval. Follows the format:
+
+                                            HH:MM
+                with
+                        HH: Hour   (from '00' to '23')
+                        MM: Minute (from '00' to '59')
+
+                Examples: '08:14', '17:25'
+    :param end_time: (str) The end time of the interval. Follows the same format as start_time.
+    :return: (tuple) Follows the format:
+
+                                    <hours>, <minutes>
+                with
+                        <hours>: (int) Number of hours elapsed
+                        <minutes>: (int) Number of minutes elapsed
+    """
+    start_time_hour_int = int(start_time.split(':')[0])
+    start_time_minute_int = int(start_time.split(':')[1])
+
+    end_time_hour_int = int(end_time.split(':')[0])
+    end_time_minute_int = int(end_time.split(':')[1])
+
+    elapsed_minutes = end_time_minute_int - start_time_minute_int
+    elapsed_hours = end_time_hour_int - start_time_hour_int
+
+    if elapsed_minutes < 0:
+        elapsed_minutes += 60
+        elapsed_hours -= 1
+
+    return elapsed_hours, elapsed_minutes
+
+
+def get_max_west_shadow_details(index, building_list, floor):
+    """
+        Given a building and a list of buildings, returns the position of the building on the west of the specified one,
+        that creates the highest shadow from the west (i.e., from overhead to sunset position of the Sun).
+
+        IMPLEMENTATION NOTE: Since this function does not return angles, and the inner usage of angles is restricted to
+        angle comparison, I will simplify angle = atan(angle), since both have the same comparison (performance reason).
+
+    :param index: (int) Position of the building in the list of buildings (0 to N-1).
+    :param building_list: (list of dict) Follows the same format as the field "buildings" within each
+        neighbourhood specified in the init API endpoint described in the Code Challenge. That is:
+
+            [{name:<name_string>, apartments_count: <number>, distance: <number>}]
+
+            As specified in the Code Challenge, it is assumed that the building list is ordered from east to west.
+
+    :param index: (int) Floor of the building to make the measurement from (0 to N-1).
+
+            NOTICE that depending on the floor height it could be a different object the one that projects its shadow
+            on it.
+
+    :return: (tuple) Following the format:
+
+                    (<max_west_shadow_index>, <max_west_shadow_distance>)
+
+                with:
+
+                    <max_west_shadow_index> : (int) Position of the building that creates the highest shadow on the
+                        west in the list of buildings (0 to N-1).
+                    <max_west_shadow_distance> : (int) Distance between the specified building and the building that
+                        creates the highest shadow on the west.
+
+    """
+    # Maximum shadow angle from the obstacles on the west.
+    max_west_shadow_angle = 0
+    # Distance to the the building that creates the maximum shadow angle on the west.
+    max_west_shadow_distance = 0
+    # Accumulated distance from the first building on the west
+    acc_west_distance = 0
+    # Index to the building that creates the maximum shadow angle on the west.
+    max_west_shadow_index = -1
+
+    for w_index in range(index, len(building_list)-1):
+        # Update distance
+        acc_west_distance += building_list[w_index]["distance"]
+        if building_list[w_index+1]["apartments_count"] - (floor + 1) >= 0:
+            #
+            # As the relative position is higher the perspective changes
+            #
+            angle = float(building_list[w_index+1]["apartments_count"] - (floor - 1)) / float(acc_west_distance)
+            if angle > max_west_shadow_angle:
+                max_west_shadow_angle = angle
+                max_west_shadow_index = w_index + 1
+                max_west_shadow_distance = acc_west_distance
+
+    if max_west_shadow_index == -1 and max_west_shadow_distance == 0:
+        # Mark "no distance" or "infinite distance" as -1
+        max_west_shadow_index = -1
+        max_west_shadow_distance = -1
+
+    return max_west_shadow_index, max_west_shadow_distance
+
+
+def get_neighbourhood_sunlight_hours(building_list, city_dawn, city_sunset):
     """
         Adds per apartment dawn and sunset info to the specified neighbourhood. That is, for each building in the
         neighbourhood two new lists will be added, dawn and sunset, that specify the time when the sunlight starts and
@@ -117,15 +218,92 @@ def get_neighbourhood_sunlight_hours(building_list):
 
             As specified in the Code Challenge, it is assumed that the building list is ordered from east to west.
 
+    :param city_dawn: (str) The local time when starts the sunlight in the city. Follows the format:
+
+                                            HH:MM
+                with
+                        HH: Hour   (from '00' to '23')
+                        MM: Minute (from '00' to '59')
+
+                Examples: '08:14', '17:25'
+
+    :param city_sunset: (str) The local time when ends the sunlight in the city. Follows the same format as city_dawn.
     :return: None
     """
-    for building in building_list:
+    # Compute the number of elapsed seconds for each grade of the sunlight, assuming (as said in the Code Challenge)
+    # that the sun rises in the east and travels at a constant radial speed until setting
+    city_sunlight_hours, city_sunlight_minutes = elapsed_time(city_dawn, city_sunset)
+
+    city_sunlight_seconds = city_sunlight_hours * 3600 + city_sunlight_minutes * 60
+    city_seconds_per_grade = float(city_sunlight_seconds)/float(180)
+
+    # Maximum shadow angle from the obstacles on the east.
+    max_east_shadow_angle = 0
+    # Distance to the the building that creates the maximum shadow angle on the east.
+    max_east_shadow_distance = 0
+    # Accumulated distance from the first building on the east
+    acc_east_distance = 0
+    # Index to the building that creates the maximum shadow angle on the east.
+    max_east_shadow_index = -1
+
+    for index, building in enumerate(building_list):
         dawn = []
         sunset = []
 
-        # This is the maximum shadow angle from the obstacles on the east.
-        max_east_shadow_angle = 0
-
         for floor in range(building["apartments_count"]):
-            #get_east_shadow_angle()
-            pass
+
+            #
+            # EAST SIDE
+            #
+
+            # Get angle of the highest shadow on the east. Notice that the higher the floor the lower the shadow from
+            # other obstacles.
+            try:
+                angle = degrees(atan(((float(building_list[max_east_shadow_index]["apartments_count"]) -
+                                      float(floor))*FLOOR_HEIGHT)/float(max_east_shadow_distance)))
+
+            except (ZeroDivisionError, IndexError):
+                # The first building has no obstacles on the east
+                angle = 0
+
+            dawn.append(get_apartment_dawn(angle, city_seconds_per_grade, city_dawn))
+            print("\nEAST ANGLE: {}, FLOOR: {}, DAWN: {}".format(angle, floor, dawn[-1]))  # DEBUGGING
+
+            #
+            # WEST SIDE
+            #
+
+            max_west_shadow_index, max_west_shadow_distance = get_max_west_shadow_details(index, building_list, floor)
+
+            # Get angle of the highest shadow on the west. Notice that the higher the floor the lower the shadow from
+            # other obstacles.
+            try:
+                angle = degrees(atan(((float(building_list[max_west_shadow_index]["apartments_count"]) -
+                                      float(floor))*FLOOR_HEIGHT)/float(max_west_shadow_distance)))
+
+            except (ZeroDivisionError, IndexError):
+                # The last building has no obstacles on the west
+                angle = 0
+
+            sunset.append(get_apartment_sunset(angle, city_seconds_per_grade, city_sunset))
+            print("\nWEST ANGLE: {},  max_west_shadow_index: {}, max_west_shadow_distance: {}, FLOOR: {}, SUNSET: {}".format(angle, max_west_shadow_index, max_west_shadow_distance, floor, sunset[-1]))  # DEBUGGING
+            print("\n-------------------------------------------------------------------------") #DEBUGGING
+
+        import ipdb; ipdb.set_trace()  # DEBUGGING
+        # Update building info
+        building["dawn"] = dawn
+        building["sunset"] = sunset
+
+        # Update distance
+        acc_east_distance += building["distance"]
+
+        # Before proceeding with the next building on the west, update the max east shadow with the current building
+        # (if proceeds)
+        angle = float(building["apartments_count"]) / float(building["distance"])
+        if angle > max_east_shadow_angle:
+            max_east_shadow_angle = angle
+            max_east_shadow_index = index
+            max_east_shadow_distance = acc_east_distance
+
+
+
--- badi/badi/tests/tests_sunlight_hours.py
+++ badi/badi/tests/tests_sunlight_hours.py
@@ -4,7 +4,8 @@
 from django.test import TestCase
 
 from ..constants import DEFAULT_CITY, DEFAULT_CITY_VALUES
-from ..sunlight_hours import get_apartment_dawn, get_apartment_sunset, get_neighbourhood_sunlight_hours
+from ..sunlight_hours import get_apartment_dawn, get_apartment_sunset, elapsed_time, get_max_west_shadow_details, \
+    get_neighbourhood_sunlight_hours
 
 
 class SunlightHoursTestCase(TestCase):
@@ -15,25 +16,80 @@ class SunlightHoursTestCase(TestCase):
         pass
 
     def test__get_apartment_dawn__ok(self):
-        angle = 22.25
-        city_seconds_per_grade = 180
+        # (<angle>, (<city_seconds_per_grade>, <expected_apartment_dawn>))
+        values = [
+            (22.25, 180, "09:20"),
+            (76.0, 180, "12:02"),
+        ]
 
         # Test main
-        apartment_dawn = get_apartment_dawn(angle, city_seconds_per_grade, DEFAULT_CITY_VALUES[DEFAULT_CITY]["dawn"])
+        for value in values:
+            apartment_dawn = get_apartment_dawn(value[0], value[1], DEFAULT_CITY_VALUES[DEFAULT_CITY]["dawn"])
 
-        # Check results
-        self.assertEqual(apartment_dawn, "09:20")
+            # Check results
+            self.assertEqual(apartment_dawn, value[2])
 
     def test__get_apartment_sunset__ok(self):
-        angle = 22.25
-        city_seconds_per_grade = 180
+        # (<angle>, (<city_seconds_per_grade>, <expected_apartment_sunset>))
+        values = [
+            (170, 180, "08:55"),
+            (22.25, 180, "16:19"),
+            (46.0, 180, "15:07"),
+        ]
 
         # Test main
-        apartment_dawn = get_apartment_sunset(angle, city_seconds_per_grade,
-                                              DEFAULT_CITY_VALUES[DEFAULT_CITY]["sunset"])
+        for value in values:
+            apartment_dawn = get_apartment_sunset(value[0], value[1], DEFAULT_CITY_VALUES[DEFAULT_CITY]["sunset"])
 
-        # Check results
-        self.assertEqual(apartment_dawn, "16:19")
+            # Check results
+            self.assertEqual(apartment_dawn, value[2])
+
+    def test__elapsed_time__ok(self):
+        values = [
+                    ("10:00", "17:00", (7, 0)),
+                    ("9:59", "17:00", (7, 1)),
+                ]
+
+        # Test main
+        for value in values:
+            hours, minutes = elapsed_time(value[0], value[1])
+
+            # Check results
+            self.assertEqual(hours, value[2][0])
+            self.assertEqual(minutes, value[2][1])
+
+    def test__get_max_west_shadow_details__ok(self):
+        building_list = [{"name": "Aticco", "apartments_count": 8, "distance": 1},
+                         {"name": "01", "apartments_count": 4, "distance": 2},
+                         {"name": "CEM", "apartments_count": 7, "distance": 1},
+                         {"name": "30", "apartments_count": 1, "distance": -1}]
+
+        # (<index>, [(<expected_max_west_shadow_index_floor_0>, <expected_max_west_shadow_distance_floor_0>)),
+        #            (<expected_max_west_shadow_index_floor_1>, <expected_max_west_shadow_distance_floor_1>)),
+        #               ...
+        #            (<expected_max_west_shadow_index_floor_N>, <expected_max_west_shadow_distance_floor_N>))
+        #           ]
+        values = [
+            (0, [(1, 1), (1, 1), (1, 1), (1, 1), (2, 3), (2, 3), (2, 3), (-1, -1)]),
+            (1, [(2, 2), (2, 2), (2, 2), (2, 2)]),
+            (2, [(3, 1), (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1), (-1, -1)]),
+            (3, [(-1, -1)]),
+        ]
+
+        # Test main
+        for value in values:
+
+            # Assure that the values are coherent. There must be as many results per building as apartments (with one
+            #  apartment per floor)
+            self.assertEqual(len(value[1]), building_list[value[0]]["apartments_count"])
+
+            for floor in range(building_list[value[0]]["apartments_count"]):
+                max_west_shadow_index, max_west_shadow_distance = get_max_west_shadow_details(value[0], building_list,
+                                                                                              floor)
+
+                # Check results
+                self.assertEqual(max_west_shadow_index, value[1][floor][0])
+                self.assertEqual(max_west_shadow_distance, value[1][floor][1])
 
     def test__get_neighbourhood_sunlight_hours__ok(self):
         building_list = [{"name": "Aticco", "apartments_count": 8, "distance": 1},
@@ -42,7 +98,8 @@ class SunlightHoursTestCase(TestCase):
                          {"name": "30", "apartments_count": 1, "distance": -1}]
 
         # Test main
-        get_neighbourhood_sunlight_hours(building_list)
+        get_neighbourhood_sunlight_hours(building_list, DEFAULT_CITY_VALUES[DEFAULT_CITY]["dawn"],
+                                         DEFAULT_CITY_VALUES[DEFAULT_CITY]["sunset"])
 
         # Check results
         expected_building_list = [
